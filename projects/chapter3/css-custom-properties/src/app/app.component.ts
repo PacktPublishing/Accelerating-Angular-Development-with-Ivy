6d6968684638b161422175608b4b60a5bd8ea4b6
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-07-29 14:58:40 +0200

    add Chapter 3 CSS Custom Properties

--- projects/chapter3/css-custom-properties/src/app/app.component.ts
+++ projects/chapter3/css-custom-properties/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <label>
+      Text size
+      <input type="number" min="10" max="48" step="2" [(ngModel)]="textSize" />
+    </label>
+    px
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan,
+      nisi sed aliquet lobortis, est lorem euismod libero, at rutrum lacus
+      tellus non metus. Proin a nunc a libero vehicula egestas pretium id ipsum.
+    </p>
+  `,
 })
 export class AppComponent {
-  title = 'chapter3-css-custom-properties';
+  @HostBinding('style.--text-size.px')
+  textSize = 16;
 }
