54a62b6bc585e0c79b253889f7defcebce7bdfa8
Jacob Andresen <jacob.andresen@gmail.com>
2021-08-02 08:12:58 +0200

    use waitForAsync on beforeEach

--- projects/demo/src/app/video/video.component.spec.ts
+++ projects/demo/src/app/video/video.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { YouTubePlayerModule } from '@angular/youtube-player';
 import { VideoComponent } from './video.component';
 
@@ -6,12 +6,13 @@ describe('VideoComponent', () => {
   let component: VideoComponent;
   let fixture: ComponentFixture<VideoComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
       imports: [YouTubePlayerModule],
       declarations: [VideoComponent],
     }).compileComponents();
-  });
+  }));
+
   it('should create', () => {
     fixture = TestBed.createComponent(VideoComponent);
     component = fixture.componentInstance;
