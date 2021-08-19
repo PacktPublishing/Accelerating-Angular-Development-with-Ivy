469e4f4a97a175977e98cdd3d49db5f5066319ca
Jacob Andresen <jacob.andresen@gmail.com>
2021-08-19 19:54:39 +0200

    async load twitter widget.js when used (#2)

--- projects/demo/src/app/tweetcourse/tweetcourse.component.ts
+++ projects/demo/src/app/tweetcourse/tweetcourse.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CourseService } from '../course.service';
 import { ICourse } from '../course.model';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'workspace-tweetcourse',
@@ -11,10 +12,24 @@ export class TweetCourseComponent implements OnInit {
   @Input()
   courseId!: string;
 
+  private twitterScript: HTMLScriptElement;
+
   public course$: Observable<ICourse> | undefined;
-  constructor(private courseService: CourseService) {}
+
+   constructor(@Inject(DOCUMENT) private document: Document,
+                private courseService: CourseService) {
+    this.twitterScript = this.document.createElement('script');
+    this.twitterScript.src = "https://platform.twitter.com/widgets.js";
+    this.twitterScript.async = true;
+  }
 
   ngOnInit(): void {
     this.course$ = this.courseService.getCourse(this.courseId);
+    this.document.body.appendChild(this.twitterScript);
   }
+
+  ngOnDestroy(): void {
+    this.document.body.removeChild(this.twitterScript);
+  }
+
 }
