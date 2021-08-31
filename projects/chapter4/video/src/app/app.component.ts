ae36034619ae2f65ecf2607cb3c5222e0e5b49fb
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-08-31 22:18:24 +0200

    feat: generate video feature app

---
+++ projects/chapter4/video/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `Video`,
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `,
+  ],
+})
+export class AppComponent {}
