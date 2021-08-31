92048b9b0cca3f6380885985a80b335562ece56a
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-08-31 22:53:52 +0200

    feat: add Angular Material theme

--- projects/chapter4/map/src/app/app.module.ts
+++ projects/chapter4/map/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MapModule } from './map/map.module';
@@ -7,6 +7,6 @@ import { MapModule } from './map/map.module';
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [BrowserModule, MapModule],
+  imports: [BrowserAnimationsModule, MapModule],
 })
 export class AppModule {}
