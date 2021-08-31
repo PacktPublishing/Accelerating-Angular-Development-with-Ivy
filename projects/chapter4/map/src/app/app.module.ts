554a5d853371f9703c7fd26d81bdf1148fcdee1e
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-08-31 22:48:57 +0200

    feat: add map component

--- projects/chapter4/map/src/app/app.module.ts
+++ projects/chapter4/map/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MapModule } from './map/map.module';
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MapModule],
 })
 export class AppModule {}
