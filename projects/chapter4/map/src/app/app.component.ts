916eb72a7b35cc5f7da7793216fa59725790344d
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-08-31 22:39:23 +0200

    feat: generate map app

---
+++ projects/chapter4/map/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `Map`,
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `,
+  ],
+})
+export class AppComponent {}
