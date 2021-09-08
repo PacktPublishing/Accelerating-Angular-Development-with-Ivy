b27c611249fd70c7d8ee33c04bb9ce649526ef85
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-08 22:41:06 +0200

    feat: generate Chapter 9 app

---
+++ projects/chapter9/random-number/src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [BrowserAnimationsModule],
+})
+export class AppModule {}
