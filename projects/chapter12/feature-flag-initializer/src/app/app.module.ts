1b12ff3d7718943ed6766d5673e4f1474e2948b3
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-09-12 21:20:39 +0200

    feat: generate chapter 12 feature app

---
+++ projects/chapter12/feature-flag-initializer/src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+})
+export class AppModule {}
